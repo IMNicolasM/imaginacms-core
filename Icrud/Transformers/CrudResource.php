3d0feca49ef203a48236633889102775ba1b665a
msolanogithub <msolano@imaginacolombia.com>
2024-08-30 17:20:58 -0500

    ICRUD:: doesn't transform model attributes if the module put it

--- Icrud/Transformers/CrudResource.php
+++ Icrud/Transformers/CrudResource.php
@@ -13,6 +13,13 @@ use  Modules\Isite\Transformers\RevisionTransformer;
 
 class CrudResource extends JsonResource
 {
+
+  /**
+   * Attribute to exclude relations from transformed data
+   * @var array
+   */
+  protected $excludeRelations = [];
+
   /**
    * Method to merge values to response
    *
@@ -30,104 +37,108 @@ class CrudResource extends JsonResource
    */
   public function toArray($request)
   {
-    $response = []; //Default Response
-    //Get translatable attributes
-    $translatableAttributes = array_filter(($this->translatedAttributes ?? []), function ($value) {
-      return !in_array($value, ['locale']);//Exclude this attributes from translatable attributes
-    });
-    $attributes = method_exists($this->resource, "getFillables") ?
-      $this->resource->getFillables() : [];//Get all fillable attributes, just for model that extends CrudModel
+      $response = []; //Default Response
 
-    $attributes = array_merge($attributes, array_keys($this->getAttributes())); //get Attributes add extras non fillables attributes
+      //Get translatable attributes
+      $translatableAttributes = array_filter(($this->translatedAttributes ?? []), function ($value) {
+        return !in_array($value, ['locale']);//Exclude this attributes from translatable attributes
+      });
+      $attributes = method_exists($this->resource, "getFillables") ?
+        $this->resource->getFillables() : [];//Get all fillable attributes, just for model that extends CrudModel
 
-    $filter = json_decode($request->filter);//Get request Filters
-    $languages = \LaravelLocalization::getSupportedLocales();// Get site languages
-    $excludeRelations = ['translations'];//No self-load this relations
+      $attributes = array_merge($attributes, array_keys($this->getAttributes())); //get Attributes add extras non fillables attributes
 
-    //Add attributes
-    foreach ($attributes as $fieldName) {
-      $response[snakeToCamel($fieldName)] = $this->when(
-        (isset($this[$fieldName]) || is_null($this[$fieldName])),
-        $this[$fieldName]
-      );
-    }
+      $filter = json_decode($request->filter);//Get request Filters
+      $languages = \LaravelLocalization::getSupportedLocales();// Get site languages
+      $excludeRelations = array_merge(['translations'], $this->excludeRelations);//No self-load this relations
 
-    //Add translatable attributes
-    foreach ($translatableAttributes as $fieldName) {
-      $response[snakeToCamel($fieldName)] = $this->when(
-        (isset($this[$fieldName]) || is_null($this[$fieldName])),
-        $this[$fieldName]
-      );
-    }
+      //Get model attribute form module
+      $response = $this->modelAttributes($request);
 
-    // Add translations
-    if (isset($filter->allTranslations) && $filter->allTranslations) {
-      foreach ($languages as $lang => $value) {
-        foreach ($translatableAttributes as $fieldName) {
-          $response[$lang][snakeToCamel($fieldName)] = $this->hasTranslation($lang) ? $this->translate($lang)[$fieldName] : '';
+      //Add attributes
+      foreach ($attributes as $fieldName) {
+        if (!in_array($fieldName, array_keys($response))) {
+          $response[snakeToCamel($fieldName)] = $this->when(
+            (isset($this[$fieldName]) || is_null($this[$fieldName])),
+            $this[$fieldName]
+          );
         }
       }
-    }
 
-    //Transform relations.
-    foreach ($this->getRelations() as $relationName => $relation) {
-      if (!in_array($relationName, $excludeRelations)) {
-        //Transform relation
-        $response[$relationName] = $this->transformData($relation);
-        //Format fields relation
-        if (($relationName == 'fields') && method_exists($this->resource, 'formatFillableToModel')) {
-          //Get fillable data
-          $fillableData = json_decode(json_encode($response[$relationName]));
-          //Merge fillable to main level of response
-          $response = array_merge_recursive($response, $this->formatFillableToModel($fillableData));
-        }
-        //Format files relations
-        if (($relationName == 'files') && method_exists($this->resource, 'mediaFiles')) {
-          //Add files relations
-          $response["files"] = MediaTransformer::collection($this->files);
-          //Add media Files
-          if (method_exists($this->resource, 'mediaFiles')) $response['mediaFiles'] = $this->mediaFiles();
-          //Add media Fields to model
-          if (method_exists($this->resource, 'getMediaFields')) {
-            $response = array_merge($response, $this->getMediaFields());
+      //Add translatable attributes
+      foreach ($translatableAttributes as $fieldName) {
+        $response[snakeToCamel($fieldName)] = $this->when(
+          (isset($this[$fieldName]) || is_null($this[$fieldName])),
+          $this[$fieldName]
+        );
+      }
+
+      // Add translations
+      if (isset($filter->allTranslations) && $filter->allTranslations) {
+        foreach ($languages as $lang => $value) {
+          foreach ($translatableAttributes as $fieldName) {
+            $response[$lang][snakeToCamel($fieldName)] = $this->hasTranslation($lang) ? $this->translate($lang)[$fieldName] : '';
           }
         }
-        //Add Revision relation
-        if (($relationName == 'revisions') && method_exists($this->resource, 'revisions')) {
-          $response['revisions'] = RevisionTransformer::collection($this->whenLoaded('revisions'));
-        }
-        //Add Tags Relation | Return only the names of tags as array
-        if (($relationName == 'tags') && method_exists($this->resource, 'getNameTags')) {
-          $response['tags'] = $this->getNameTags();
+      }
+
+      //Transform relations.
+      foreach ($this->getRelations() as $relationName => $relation) {
+        if (!in_array($relationName, $excludeRelations)) {
+          //Transform relation
+          $response[$relationName] = $this->transformData($relation);
+          //Format fields relation
+          if (($relationName == 'fields') && method_exists($this->resource, 'formatFillableToModel')) {
+            //Get fillable data
+            $fillableData = json_decode(json_encode($response[$relationName]));
+            //Merge fillable to main level of response
+            $response = array_merge_recursive($response, $this->formatFillableToModel($fillableData));
+          }
+          //Format files relations
+          if (($relationName == 'files') && method_exists($this->resource, 'mediaFiles')) {
+            //Add files relations
+            $response["files"] = MediaTransformer::collection($this->files);
+            //Add media Files
+            if (method_exists($this->resource, 'mediaFiles')) $response['mediaFiles'] = $this->mediaFiles();
+            //Add media Fields to model
+            if (method_exists($this->resource, 'getMediaFields')) {
+              $response = array_merge($response, $this->getMediaFields());
+            }
+          }
+          //Add Revision relation
+          if (($relationName == 'revisions') && method_exists($this->resource, 'revisions')) {
+            $response['revisions'] = RevisionTransformer::collection($this->whenLoaded('revisions'));
+          }
+          //Add Tags Relation | Return only the names of tags as array
+          if (($relationName == 'tags') && method_exists($this->resource, 'getNameTags')) {
+            $response['tags'] = $this->getNameTags();
+          }
         }
       }
-    }
 
+      //Add magic attributes
+      foreach (get_class_methods($this->resource) as $methodName) {
+        // if the method starts with get and ends with Attribute
+        // excepting base method "getAttribute"
+        if (!in_array($methodName, array_keys($response))) {
+          if (Str::startsWith($methodName, "get") && Str::endsWith($methodName, "Attribute")
+            && $methodName != "getAttribute") {
+            //removing "get" and "Attribute" to get the real attribute name
+            $attributeName = Str::replace(["get", "Attribute"], ["", ""], $methodName);
 
-    //Add magic attributes
-    foreach (get_class_methods($this->resource) as $methodName) {
-      // if the method starts with get and ends with Attribute
-      // excepting base method "getAttribute"
-      if (Str::startsWith($methodName, "get") && Str::endsWith($methodName, "Attribute")
-        && $methodName != "getAttribute") {
-        //removing "get" and "Attribute" to get the real attribute name
-        $attributeName = Str::replace(["get", "Attribute"], ["", ""], $methodName);
-
-        //avoid the magic methods of the fillables
-        if (!in_array(Str::snake($attributeName), array_merge($attributes, $translatableAttributes))) {
-          $response[Str::camel($attributeName)] = $this->{$methodName}();
+            //avoid the magic methods of the fillables
+            if (!in_array(Str::snake($attributeName), array_merge($attributes, $translatableAttributes))) {
+              $response[Str::camel($attributeName)] = $this->{$methodName}();
+            }
+          }
         }
       }
-    }
 
+      //Sort response
+      ksort($response);
 
-    //Add model extra attributes
-    $response = array_merge($response, $this->modelAttributes($request));
-    //Sort response
-    ksort($response);
-
-    //Response
-    return $response;
+      //Response
+      return $response;
   }
 
   /**
