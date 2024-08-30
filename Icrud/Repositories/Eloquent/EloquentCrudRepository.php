c2260d8cecdf1f39722e56a7064d333dd0b1d9ec
Cristian Marin <marin101998@gmail.com>
2024-08-30 16:01:54 -0500

    + fix | v8 30-08-24 change privacy function

--- Icrud/Repositories/Eloquent/EloquentCrudRepository.php
+++ Icrud/Repositories/Eloquent/EloquentCrudRepository.php
@@ -775,11 +775,11 @@ abstract class EloquentCrudRepository extends EloquentBaseRepository implements 
    *
    * @param $params
    */
-  private function compareParameters($params): bool
+  public function compareParameters($params)
   {
     $newParams = json_encode($params);
     $queryParams = json_encode($this->params);
-    return $newParams != $queryParams;;
+    return $newParams != $queryParams;
   }
 
   private function hasSoftDeletes()
