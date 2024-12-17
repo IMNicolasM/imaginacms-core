c92ab46c1ab575304a808a6fe116bec4501115f1
William <williamverde10@gmail.com>
2024-12-17 11:56:00 -0400

    Update: uncommented generateTestingData

--- Icrud/Controllers/BaseCrudController.php
+++ Icrud/Controllers/BaseCrudController.php
@@ -312,12 +312,10 @@ class BaseCrudController extends BaseApiController
       $params = $this->getParamsRequest($request);
 
       //Only in dev mode
-      /*
       if(app()->environment('local')){
         if(isset($params->filter) && isset($params->filter->generateTestingData) && $params->filter->generateTestingData)
           $items = generateTestingData($params->filter->generateTestingData);
       }
-      */
       
       //Init Service
       $bulkService = app()->makeWith(BulkService::class,['params' => ['controller'=> $this, 'items'=> $items]]);
